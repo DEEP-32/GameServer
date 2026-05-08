1f88dc7d94b77e3c949c03a04d19fe236ce2546c
Deepanshu Singh <anshu3012ep@gmail.com>
2026-05-08 17:51:17 +0530

    add classes

---
+++ Server/Controllers/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Services;
+using SharedLibrary;
+
+namespace Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PlayerController  : ControllerBase{
+    
+    private readonly PlayerService _playerService;
+    
+    public PlayerController(PlayerService playerService) {
+        _playerService = playerService;
+    }
+    
+    [HttpGet("{id}")]
+    public Player Get([FromRoute] int id) {
+        var player = new Player() {
+            Id = id
+        };
+        
+        _playerService.DoSomething();
+        return player;
+    }
+
+
+    [HttpPost]
+    public Player Post(Player player) {
+        Console.WriteLine("Player has beed added to database");
+        return player;
+    }
+        
+}
---
+++ Server/Services/PlayerService.cs
@@ -0,0 +1,14 @@
+﻿namespace Server.Services;
+
+
+public interface IPlayerService {
+    void DoSomething();
+}
+
+
+public class PlayerService : IPlayerService{
+    
+    public void DoSomething() {
+        Console.WriteLine("PlayerService : hey!");
+    }
+}
